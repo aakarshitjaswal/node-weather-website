ce57fc80d4661839193c938a37faf6af1c865d31
Aakarshit Jaswal <aakarshit@cqlsys.co.uk>
2020-09-09 13:39:21 +0530

    Setup app for heroku

--- src/app.js
+++ src/app.js
@@ -5,6 +5,8 @@ const path = require('path')
 const hbs = require('hbs')
 
 const app = express()
+const port = process.env.PORT || 3000
+
 const publicDirectoryPath = path.join(__dirname,'../public' )
 const viewDirectoryPath = path.join(__dirname,'../templates/views' )
 const partialsPath = path.join(__dirname,'../templates/partials' )
@@ -81,6 +83,6 @@ app.get('*',(req, res) => {
 
 
 
-app.listen(3000, () => {
-  console.log('The server is up on port 3000.')
+app.listen(port, () => {
+  console.log('The server is up on port' + port)
 })
